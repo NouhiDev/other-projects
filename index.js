13d26f497f57385b80539840d5894a2bb48ef91d
Nouhi <67205946+NouhiDev@users.noreply.github.com>
2023-01-24 16:45:16 +0100

    Restyling

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-$(window).on("load",async function(){
-    // await delay(2000);
-    $(".loader").fadeOut(1000);
-    delay(1000).then(() => $(".content").fadeIn(1000));
-})
+$(window).on("load", async function () {
+  // await delay(2000);
+  $(".loader").fadeOut(1000);
+  delay(1000).then(() => $(".content").fadeIn(1000));
+});
+
+// Hover Effect
+document.getElementById("cards").onmousemove = (e) => {
+  for (const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+};
 
 function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 function back_btn() {
-    location.href = "https://nouhidev.github.io";
-}
-
-function school_projects_btn() {
-    location.href = "https://nouhidev.github.io/school-projects/";
+  location.href = "https://nouhi.dev/";
 }
 
 function sa_btn() {
-    location.href = "https://nouhidev.github.io/seismic-architect/";
+  location.href = "https://nouhidev.github.io/seismic-architect/";
 }
 
 function mg_btn() {
-    location.href = "https://nouhi.dev/marble-graphs/";
+  location.href = "https://nouhi.dev/marble-graphs/";
 }
 
 function es_btn() {
-    location.href = "https://nouhidev.github.io/epidemic-simulation/";
+  location.href = "https://nouhidev.github.io/epidemic-simulation/";
 }
